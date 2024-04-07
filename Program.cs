16b88bb83c10878deb9e4a0a5b6d229b961e6e7c
Dmitriy <dmitriykharitonov333@gmail.com>
2024-04-07 21:23:39 +0300

    Написал код в файле Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// Добавил блок-схему
-// Добавил файл README: заполнил в нем информацию про Блок-схемму, Алгоритм и Код на языке C#
+﻿string[] ThreeSymbolsArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] newArray = new string[count];
+
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+Console.Clear();
+
+string[] array = { "dog", "zebra", "bat", "giraffe", "elephant", "cat", "bear", "fox" };
+string[] newArray = ThreeSymbolsArray(array);
+
+Console.WriteLine($"Исходный массив:\t[" + string.Join(", ", array) + "]");
+Console.WriteLine($"Преобразованный массив:\t[" + string.Join(", ", newArray) + "]");
 
